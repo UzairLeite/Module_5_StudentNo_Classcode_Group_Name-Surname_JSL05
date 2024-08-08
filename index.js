cfe41aed21fdffe1495afd0d1dd09bcba93a4004
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2024-08-08 21:42:25 +0200

    Playlist generation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const songs = [
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
     // Feel free to add even more songs
-    { title: "I Want You Back", artist: "Jackson 5", genre: "Pop"},
+    { title: "Super Freak", artist: "Rick James", genre: "R&B"},
     { title: "Mr. Blue Sky", artist: "Electric Light Orchestra", genre: "Pop"},
     { title: "Badlands", artist: "Bruce Springsteen", genre: "Rock  "},
     { title: "Ain't No Sunshine", artist: "Bill Withers", genre: "R&B"},
@@ -22,15 +22,26 @@ const songs = [
 
 // Object containing each Guardian's preferred genre
 const guardians = {
-    "Star-Lord": "Rock",
-    "Gamora": "Pop",
+    "Star-Lord": "Pop",
+    "Gamora": "Rock",
     // Add preferences for Drax, Rocket, and Groot
+    "Drax": "Rock",
+    "Rocket": "R&B",
+    "Groot": "R&B",
 };
 
 // Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
     // Use the map() function to create playlists for each Guardian
-    // Your code here
+    return guardians.map(guardians => {
+        const playlist = songs.filter(song => sessionStorage.genre === guardian.prefferedGenre);
+
+        // Return an object with the guardian's name and their playlist
+        return {
+            guardianName: guardian.name,
+            playlist: playlist
+        };
+    });
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
